1ad16c38b8afdb4b45182194c72a2bf3c9d47a20
kdaudel <60135745+kdaudel@users.noreply.github.com>
2020-12-14 11:09:47 +0100

    Update AlphaGammaDescent.py

--- python/bayesian_logistic_regression/AlphaGammaDescent.py
+++ python/bayesian_logistic_regression/AlphaGammaDescent.py
@@ -152,13 +152,12 @@ class AlphaGammaDescent:
     def AlphaGammaDescent_algorithm(self, thetas, weights_init):
 
         self.eta_n = self.eta_n0
-        weights_sum = weights_init.copy()
         weights, llh, renyi_bound = self._weights_update(weights_init, thetas)
         renyi_bound_lst = [renyi_bound]
         llh_lst = [llh]
 
         n_iter = 1
-        weights_sum += weights.copy()
+        weights_sum = self.eta_n * weights.copy()
 
         while n_iter < self.N:
             self.eta_n = self.eta_n0 / (1 + np.sqrt(n_iter))
